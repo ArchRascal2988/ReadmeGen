f655dd9c525132c9b04449629ec094a9e776efbc
Lex Matthews <tanukiguernica@gmail.com>
2022-04-19 17:20:08 -0400

    included packages

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // TODO: Include packages needed for this application
-
+const fs= require("fs");
+const path= require("path");
+const inq= require("inquirer");
+const gMark= require("./utils/generateMarkdown")
 // TODO: Create an array of questions for user input
 const questions = [];
 
