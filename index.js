6c0a61f2458d0bdbfdfccb7b6cce680549f59470
Lex Matthews <tanukiguernica@gmail.com>
2022-04-19 19:16:17 -0400

    finished necessary code for index.js

--- index.js
+++ index.js
@@ -4,13 +4,89 @@ const path= require("path");
 const inq= require("inquirer");
 const gMark= require("./utils/generateMarkdown")
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is your github Username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "Name of project/repo?",
+        name: "reponame"
+    },
+    {
+        type: "input",
+        message: "Project description?",
+        name: "description"
+    },
+    {
+        type: "checkbox",
+        message: "Languages used?",
+        name: "lang",
+        choices: ["HTML","CSS","Javascript","MySQL","PHP","Python","C++"]
+    },
+    {
+        type: "input",
+        message: "Any installation instructions?",
+        name: "instructions"
+    },
+    {
+        type: "input",
+        message: "Want to detail usage information?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "Any contribution guidelines?",
+        name: "contributions"
+    },
+    {
+        type: "input",
+        message: "Any test instructions?",
+        name: "test"
+    },
+    {
+        type: "list",
+        message: "License used?",
+        name: "license",
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+    },
+    {
+        type: "input",
+        message: "Want to add a link?",
+        name: "link"
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inq.prompt(
+        {
+        type: "confirm",
+        message: "This is a readme markdown generator, you'll be prompted with a series of questions to fill it out. If you don't want a specific area filled out leave the prompt blank. Confirm to start or decline to exit.",
+        name: "truthy"
+        }
+    ).then((response) =>{
+        if(response.truthy){
+            inq.prompt(questions).then((responses)=>{
+                let dataObj= {};
+                let resKeys= Object.keys(responses);
+                for(const key of resKeys){
+                    dataObj[`${key}`]= responses[`${key}`];
+                }
+                gMark(dataObj);
+            })
+        } else{
+            return;
+        }
+    })
+}
 
 // Function call to initialize app
 init();
